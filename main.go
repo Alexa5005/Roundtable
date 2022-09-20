62b6691ffe663320b53fe4904cbd4b89b44e12fc
Alexa5005 <askostenko5000@mail.ru>
2022-09-20 15:07:13 +0300

    Add registration on roundtable

--- main.go
+++ main.go
@@ -7,21 +7,41 @@ import ("fmt"
 				_ "github.com/go-sql-driver/mysql"
 )
 
-type Mero struct {
-	Id uint16
-	Title, Organization, Description, Date1, Date2 string
-}
+// type Mero struct {
+// 	Id uint16
+// 	Title, Organization, Description, Date1, Date2 string
+// }
+//
+// // var lineMero = []Mero{}
 
-var lineMero = []Mero{}
-
-func index(w http.ResponseWriter, r *http.Request)  {
-	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html","templates/create.html","templates/MenuOrg.html")
+func current(w http.ResponseWriter, r *http.Request)  {
+	t, err := template.ParseFiles("templates/current.html", "templates/header.html", "templates/footer.html","templates/create.html")
 
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
 	}
 
-	t.ExecuteTemplate(w, "index", nil)
+	t.ExecuteTemplate(w, "current", nil)
+}
+
+func success(w http.ResponseWriter, r *http.Request)  {
+	t, err := template.ParseFiles("templates/success.html", "templates/header.html", "templates/footer.html","templates/create.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "success", nil)
+}
+
+func roundtable(w http.ResponseWriter, r *http.Request)  {
+	t, err := template.ParseFiles("templates/roundtable.html", "templates/header.html", "templates/footer.html","templates/create.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "roundtable", nil)
 }
 
 func create(w http.ResponseWriter, r *http.Request)  {
@@ -39,52 +59,44 @@ func save_article(w http.ResponseWriter, r *http.Request){
 		fmt.Println(err)
 		return
 	}
-	// title := r.FormValue("title")
-	// organization := r.FormValue("organization")
-	// description := r.FormValue("description")
-	// date1 := r.FormValue("date1")
-	// date2 := r.FormValue("date2")
 
-	db, err := sql.Open("mysql", "kostenko:qwerty123@tcp(172.18.1.25:3306)/Mero")
+	lastName := r.FormValue("lastName")
+	firstName := r.FormValue("firstName")
+	middleName := r.FormValue("middleName")
+	organization := r.FormValue("organization")
+	position := r.FormValue("position")
+	phone := r.FormValue("phone")
+	email := r.FormValue("email")
+
+
+
+	db, err := sql.Open("mysql", "kostenko:qwerty123@tcp(172.18.1.25:3306)/Roundtable")
 	if err != nil {
 		panic(err)
 	}
 
 	// Установка данных
-	// insert, err := db.Query(fmt.Sprintf("INSERT INTO CreateEvents (title, organization, description, date1, date2) VALUES ('%s', '%s', '%s', '%s', '%s')", title, organization, description, date1, date2))
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer insert.Close()
+	insert, err := db.Query(fmt.Sprintf("INSERT INTO Roundtable (lastName, firstName, middleName, organization, position, phone, email) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s')", lastName, firstName, middleName, organization, position, phone, email))
+	if err != nil {
+		panic(err)
+	}
+	defer insert.Close()
 
 	defer db.Close()
 
-	// //Создание соединения с базой данных
-	// 	db, err := sql.Open(typeBase, adressBase)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	//
-	// 	// Установка данных
-	// 	insert, err := db.Query(fmt.Sprintf("INSERT INTO" + NameTable +"(" + arg1 + ", " + arg2 + ", " + arg3 + ", " + arg4 + ", " + arg5 + ") VALUES ('%s', '%s', '%s', '%s', '%s')", arg6, arg7, arg8, arg9, arg10))
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	defer insert.Close() //остановка записи в базу
-	//
-	// 	defer db.Close() //Остановка соединения с базой
-
-
-
-	http.Redirect(w, r, "/badge", http.StatusSeeOther)
+	http.Redirect(w, r, "/success", http.StatusSeeOther)
 }
 
 func handleFunc()  {
 	http.Handle("/static", http.StripPrefix("/static/", http.FileServer(http.Dir("/static/"))))
-	http.HandleFunc("/", index)
+	http.HandleFunc("/", current)
+	http.HandleFunc("/current", current)
 	http.HandleFunc("/create", create)
+	http.HandleFunc("/success", success)
 	http.HandleFunc("/save_article", save_article)
+	http.HandleFunc("/roundtable", roundtable)
 	http.ListenAndServe("172.18.1.25:8080", nil)
+
 }
 
 func main() {
